6d9a0a7aab7fd3d3a3bc23a2c25e0df31f70b52e
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-09-27 15:56:34 +0200

    Pagina_web_Script

---
+++ cliente/js/main.js
@@ -0,0 +1,37 @@
+function desvanecerTexto() {
+    var txt = document.getElementsByClassName("texto-fotos")[0];
+    txt.style.opacity = "0";
+    txt.style.transition = ".4s";
+}
+function aparecer() {
+    var txt = document.getElementsByClassName("texto-fotos")[0];
+    txt.style.opacity = "1";
+    txt.style.transition = ".4s";
+}
+function desvanecerTexto2() {
+    var txt2 = document.getElementsByClassName("texto-fotos-seccion-2")[0];
+    txt2.style.opacity = "0";
+    txt2.style.transition = ".4s";
+}
+function aparecer2() {
+    var txt2 = document.getElementsByClassName("texto-fotos-seccion-2")[0];
+    txt2.style.opacity = "1";
+    txt2.style.transition = ".4s";
+}
+function desvanecerTexto3() {
+    var txt2 = document.getElementsByClassName("texto-fotos-seccion-3")[0];
+    txt2.style.opacity = "0";
+    txt2.style.transition = ".4s";
+}
+function aparecer3() {
+    var txt2 = document.getElementsByClassName("texto-fotos-seccion-3")[0];
+    txt2.style.opacity = "1";
+    txt2.style.transition = ".4s";
+}
+function comprar() {
+    alert("Directico a instantgaming");
+}
+function info() {
+    alert("pa la web oficial monstro");
+
+}
