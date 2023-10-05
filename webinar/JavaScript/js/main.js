66ecb86d500219bc8bf5b3d5a3e404af63c888a5
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-10-06 01:12:27 +0200

    imagenes_ajustadas

---
+++ webinar/JavaScript/js/main.js
@@ -0,0 +1,32 @@
+/*KATA 1 hoisting*/
+
+var country = "espiña";
+const year = "2023";
+console.log(country,year);
+
+/*KATA2*/
+let string = "calle recta , 23";
+console.log(typeof string);
+let numero = 12;
+console.log(typeof numero);
+let boolean = true;
+console.log(typeof boolean);
+
+/*funciones*/
+function printGreet(){
+    return "Hello"
+}
+let resul = printGreet();
+console.log(resul);
+
+function getCopy(){
+    let copy = "alvaro - olmos- Sierra";
+    return copy;
+}
+let resul2 = getCopy();
+console.log(resul2);
+
+
+
+
+
