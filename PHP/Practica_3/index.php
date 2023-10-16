26cf6587d92c992075bdc9453735df1bf3a756d4
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-10-16 15:03:28 +0200

    arreglos_php y borrado de error cliente

--- PHP/Practica_3/index.php
+++ PHP/Practica_3/index.php
@@ -4,11 +4,12 @@
 
     if ($precio <= 50){
         $precio += 3.95;
-    }else if ($precio > 50 && $precio < 75){
+    }else if ($precio > 50 && $precio <= 75){
         $precio += 3.95;
-    }else if ($precio > 75 && $precio < 100){
+    }else if ($precio > 75 && $precio >= 100){
         $precio += 1.95;
     }else if($precio > 100){
         echo "envio gratis";
     }
+    
 ?>
