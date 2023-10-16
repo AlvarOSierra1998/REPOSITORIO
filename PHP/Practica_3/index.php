70e786795e4f3f2e8da36ef85ebfcdc9e16b8563
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-10-16 14:35:03 +0200

    continuacion_php

--- PHP/Practica_3/index.php
+++ PHP/Practica_3/index.php
@@ -7,6 +7,8 @@
     }else if ($precio > 50 && $precio < 75){
         $precio += 3.95;
     }else if ($precio > 75 && $precio < 100){
-        $precio
+        $precio += 1.95;
+    }else if($precio > 100){
+        echo "envio gratis";
     }
 ?>
