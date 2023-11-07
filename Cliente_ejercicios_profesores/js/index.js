b08facdb9d6f53e290f4dd699c9b5201fe1d5314
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-11-07 21:15:49 +0100

    cliente

---
+++ Cliente_ejercicios_profesores/js/index.js
@@ -0,0 +1,54 @@
+
+var Tutor = {
+    Nombre : "evelyn",
+    edad : "28",
+    Dni : "94730263G",
+    Titulo_universitario : "ingenieria informatica",
+};
+
+//ASIGNATURA
+var Interfaces = {
+    nombre : "Interfaces webs",
+    curso : 2,
+    horas : 4
+}
+var Cliente = {
+    nombre : "Cliente",
+    curso : 2,
+    horas : 6
+}
+
+var Despliegue_servidor = {
+    nombre : "Despliegue servidor",
+    curso : 2,
+    horas : 6
+}
+
+var Despliegue_web = {
+    nombre : "Despliegue web",
+    curso : 2,
+    horas : 4
+}
+
+let asignatura = {
+    Despliegue_servidor,
+    Interfaces,
+    Cliente,
+    Despliegue_web,
+}
+
+var Alumno={
+    Nombre : "Alvaro",
+    Edad : 24,
+    Ciclo : "DAW",
+    Curso : 2,
+    Tutor : "Evelyn",
+    Asignaturas : asignatura,
+    Nota_media : notas[5], 
+}
+
+var Nota_media = {
+    alumno: Alumno,
+    notas : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+}
+
