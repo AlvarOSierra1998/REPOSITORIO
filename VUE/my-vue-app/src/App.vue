8434493885d9aa4452679346ac6ce9d9edc34129
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-11-22 20:33:13 +0100

    vue

---
+++ VUE/my-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div>
+    <h1 v-bind:class="selector">Hola {{ nombre }}</h1>
+    <p>mi edad es {{edad}}</p>
+  </div>
+</template>
+
+<style scoped >
+.azul{
+  color: blue;
+}
+</style>
+
+<script setup>
+const nombre = "Alvaro";
+const edad = 18
+const color =  "color:green;font-size:3em;"
+const selector = "azul"
+</script>
