d1808ef210721dc412eaa407d3e1bdc6629073e4
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-11-23 21:05:34 +0100

    Jquery

--- VUE/my-vue-app/src/App.vue
+++ VUE/my-vue-app/src/App.vue
@@ -20,7 +20,33 @@ const selector = "azul"
 
 const saludar = (miNombre)=>{
   alert(`Hola ${miNombre}`)
+  
 }
 </script>
 
 //https://www.youtube.com/watch?v=7f7nNgZOAUc
+
+
+/***
+
+beforeCreate
+Es el primer estado del ciclo de vida que se ejecuta. En este caso, nuestro componente se ha instanciado, pero no se ha añadido reactividad, ni inicializado las propiedades data
+created:
+En este caso, ya se ha creado el componente y se ha añadido reactividad. Buen momento para hacer una petición a un servicio externo, por ejemplo.
+beforeMount:
+En este hook, ya tenemos el template de nuestro componente compilado en la función render,
+mounted:
+Se considera que el componente está montado cuando se genera la propiedad $el, es decir, nuestro componente ha sido renderizado en el DOM. Otro buen momento para hacer una petición a un servicio externo.
+beforeUpdate:
+Se lanza cuando el componente detecta un cambio en sus datos, pero aún nuestro componente no ha sido renderizado de nuevo.
+updated:
+Se ha realizado un cambio en algún dato de nuestro componente, y se ha realizado el renderizado de nuevo en el Virtual DOM.
+beforeDestroy:
+Hook que se lanza cuando Vue detecta que el componente debe ser eliminado. Este hook se ejecuta antes de eliminar todas las referencias internas del componente.
+destroyed:
+El componente va a ser eliminado, y se han eliminado todas las referencias a los watchers, y listeners.
+
+
+
+*/
+
