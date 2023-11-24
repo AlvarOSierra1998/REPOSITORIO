2265c6cbb2bbecde11c88bdfcebaab5a96aa7496
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2023-11-25 00:47:17 +0100

    jquery y contador con Vue

--- VUE/my-vue-app/src/App.vue
+++ VUE/my-vue-app/src/App.vue
@@ -2,21 +2,46 @@
   <div>
     <h1 :class="selector">Hola {{ nombre }}</h1>
     <p>mi edad es {{edad}}</p>
+
     <button @click="saludar(nombre)" >Aceptar</button>
+    <button @click="count()">contador</button>
+    <span> {{ numero}}</span>
+
+    
   </div>
 </template>
 
 <style scoped >
 .azul{
   color: rgb(24, 86, 193);
+  font-size: 4rem;
 }
 </style>
+
+<script>
+  
+export default {
+  data() {
+    return {
+      numero: 0,
+    };
+  },
+  methods: {
+    count() {
+      this.numero += 1;
+    },
+  },
+};
+</script>
+
+
 
 <script setup>
 const nombre = "Alvaro";
 const edad = 18
 const color =  "color:green;font-size:3em;"
-const selector = "azul"
+const selector = "azul";
+
 
 const saludar = (miNombre)=>{
   alert(`Hola ${miNombre}`)
