a33c72a5c238e7da16fa0fcb354d675f9355e51f
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-01-16 21:12:31 +0100

    cliente AJAX

---
+++ AJAX_CLIENTE/php_datos/datos.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_REQUEST['dato1'] == 1){
+    echo "
+    <section class = 'container'>
+     <blockquote>
+    <p>Siegmeyer of Catarina</p>
+        <p>
+            Siegmeyer es un caballero de Catarina que lleva la armadura de cebolla,
+            este ha perdido a su hija y va por el mundo buscándola a ella y también por aventuras. 
+            Hay varios momentos/lugares a lo largo del juego en el que encontrarlos y se harán mención de todos ellos, 
+            donde encontrarlo y lo que hacer en todo caso.
+        </p>
+    </blockquote>
+    </section>";
+
+}
+
+
+
+?>
