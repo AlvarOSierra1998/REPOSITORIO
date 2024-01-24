86e338fade49ef53faf36863acf085ea68985514
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-01-24 01:09:30 +0100

    Prueba metodo_random

--- TypeScript/PRACTICA_TS/index.ts
+++ TypeScript/PRACTICA_TS/index.ts
@@ -7,10 +7,29 @@ class Jugador{
     constructor(nombre:string){
         this.nombre = nombre; 
     }
+    /*REPASAR
+    calcularFuerzaInicial(min:number,max:number){
+        return()=>{
+            Math.random()*this.puntos_ataque*(max-min) + min;
+        }
     
+        
+    }*/
 }
+
+/*REPASAR
+const jugador = new Jugador("Alvaro");
+const calcularFuerza = jugador.calcularFuerzaInicial(5, 10);
+const fuerzaAleatoria = calcularFuerza();
+console.log(fuerzaAleatoria); // Imprime un valor aleatorio dentro del rango (5, 10)*/
+
 
 class Enemigo{
     private nombre:string;
     private puntos_ataque:number;
 }
+
+
+
+
+
