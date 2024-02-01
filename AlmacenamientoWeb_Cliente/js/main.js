77a2177567076ff88c39be6f4589e7a33ee820fc
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-02-01 19:31:11 +0100

    Cliente_Storage

---
+++ AlmacenamientoWeb_Cliente/js/main.js
@@ -0,0 +1,104 @@
+
+
+
+const almacenar = () => {
+
+    let userInput = prompt("Que Casco quieres buscar?");
+
+    
+    
+    let almete = { nombre: "Almete", Siglo: "XV", origen: " Italia, Francia, Inglaterra y España", img: "https://upload.wikimedia.org/wikipedia/commons/1/11/ClassicArmet_by_Emmanuel_Viollet-le-Duc.jpg" }
+    let borgoñota = { nombre: "Borgoñota", Siglo: "XVI & XVII", origen: "Region de Borgoña(Francia)", img: "https://i.pinimg.com/736x/29/2c/1f/292c1f15f5dc9dafbd8e3aa10f31d569--los.jpg" }
+    let bacinete = { nombre: "Bacinete", Siglo: "XIII", origen: "Alemania", img: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Dictionnaire_raisonn%C3%A9_du_mobilier_fran%C3%A7ais_de_l%E2%80%99%C3%A9poque_carlovingienne_%C3%A0_la_Renaissance%2C_tome_5_-_183.png" }
+    
+    let resultado = document.getElementById('res');
+    
+    /*if (!localStorage.getItem('res', resultado)) {
+        localStorage.setItem('res', resultado, JSON.stringify());
+    }
+    console.log(localStorage.getItem('res', resultado))*/
+
+    ;
+
+
+    //BORGOÑOTA
+    if (userInput === "Borgoñota") {
+        
+        localStorage.setItem('dataBorgoñota', JSON.stringify(borgoñota));
+        let StorageDataBorgo = localStorage.getItem('dataBorgoñota');
+        let ObtainedItemBorgo = JSON.parse(StorageDataBorgo);
+        console.log('ObtainedBorgo', ObtainedItemBorgo);
+        //MOSTRAR LOS ELEMENTOS DE LA BORGOÑOTA EN UN DIV
+        let MostrarElemento = document.createElement('div');
+        MostrarElemento.innerHTML = `
+        <h1>${ObtainedItemBorgo.nombre}</h1>
+        <ul>
+            <li>Siglo: ${ObtainedItemBorgo.Siglo}</li>
+            <li>Origen: ${ObtainedItemBorgo.origen}</li>
+        </ul>
+        <img src="${ObtainedItemBorgo.img}" alt="Imagen de ${ObtainedItemBorgo.nombre}">
+
+        `;
+        resultado.appendChild(MostrarElemento);
+
+        //////////////////////////////////////////////
+
+    } else if (userInput === "Almete") {
+        //ALMETE
+        
+        localStorage.setItem('dataAlmete', JSON.stringify(almete));
+        let StorageDataAlmete = localStorage.getItem('dataAlmete');
+        let ObtainedItemAlmete = JSON.parse(StorageDataAlmete);
+        console.log('ObtainedAlmete', ObtainedItemAlmete);
+        //MOSTRAR LOS ELEMENTOS DE LA BORGOÑOTA 
+        let MostrarElemento = document.createElement('div');
+        MostrarElemento.innerHTML = `
+        <h1>${ObtainedItemAlmete.nombre}</h1>
+        <ul>
+            <li>Siglo : ${ObtainedItemAlmete.Siglo}</li>
+            <li>Origen:${ObtainedItemAlmete.origen}</li>
+        </ul>
+        <img src="${ObtainedItemAlmete.img}" alt="Imagen de ${ObtainedItemAlmete.nombre}">
+        `;
+        resultado.appendChild(MostrarElemento);
+        ///////////////////////////////////////////
+    } else if (userInput === "Bacinete") {
+        //BACINET
+       
+        localStorage.setItem('dataBacinete', JSON.stringify(bacinete));
+        let StorageDataBacinete = localStorage.getItem('dataBacinete');
+        let ObtainedItemBacinet = JSON.parse(StorageDataBacinete);
+        console.log('ObtainedBaci', ObtainedItemBacinet);
+
+        let MostrarElemento = document.createElement('div');
+        MostrarElemento.innerHTML = `
+        <h1>${ObtainedItemBacinet.nombre}</h1>
+        <ul>
+            <li>Siglo: ${ObtainedItemBacinet.Siglo}</li>
+            <li>Origen: ${ObtainedItemBacinet.origen}</li>
+        </ul>
+        <img src="${ObtainedItemBacinet.img}" alt= "Imagen de ${ObtainedItemBacinet.nombre}">
+        `;
+        resultado.appendChild(MostrarElemento);
+    } else {
+        console.log("el item no esta en la lista");
+        return;
+    }
+
+
+}
+const mostrarContenido = () => {
+
+    let storedData = localStorage.getItem('storedData');
+    let storedObjects = storedData ? JSON.parse(storedData) : [];
+    localStorage.setItem('storedData', JSON.stringify(storedObjects));
+}
+
+const eliminarOne = () => {
+
+
+}
+const limpiarLista = () => {
+
+
+}
