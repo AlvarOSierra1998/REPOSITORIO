03352ce93b28acd719d17165d2ead5281ada69e4
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-02-13 00:31:23 +0100

    boton y creacion de Vane

--- VUE/my-vue-app/src/components/characters.vue
+++ VUE/my-vue-app/src/components/characters.vue
@@ -10,7 +10,7 @@
                 <button @click="mostrarVaseraga" :class="{ vaseragaB: mostrarVaseraga }">
                     <h2>VASERAGA</h2>
                 </button>
-                <button @click="mostrarVane" :class="{ vaseragaB: mostrarVane }">
+                <button @click="mostrarVane" :class="{ vaneB: mostrarVane }">
                     <h2>Vane</h2>
                 </button>
                 <button @click="mostrarKatalina" :class="{ vaseragaB: mostrarKatalina }">
@@ -58,7 +58,7 @@
                 <button @click="mostrarCagli" :class="{ vaseragaB: mostrarCagli }">
                     <h2>Cagliostro</h2>
                 </button>
-                
+
             </div>
             <div v-if="mostrarInfo" :class="{ vaseragaInfo: mostrarInfo }">
                 <img class="imgvas" src="../assets/img/vaseraga.png" alt="Vaseraga">
@@ -99,7 +99,7 @@
                                         contractor.
                                     </td>
                                     <td>
-                                        <img class="grynoth" src="../assets/img/Grynoth.png" alt="Grynoth">
+                                        <img class="weapon" src="../assets/img/Grynoth.png" alt="Grynoth">
                                     </td>
                                 </tr>
                                 <tr>
@@ -135,19 +135,20 @@
     <!---VANE---->
     <section>
         <!--lista perosnajes-->
-        
-    <!---VANE---->
+
+        <!---VANE---->
         <div>
-            
-            <div v-if="mostrarInfoV" :class="{ vaseragaInfo: mostrarInfoV }">
+            <!---es una clase el vaseraga info-->
+            <div v-if="mostrarInfoV" :class="{ VaneInfo: mostrarInfoV }">
                 <img class="imgvas" src="../assets/img/Vane.png" alt="Vane">
                 <blockquote class="txtVa">
                     <p>
                         <q>
                             <i>
-                                This towering Draph warrior belongs to the Society, an organization which hunts primal
-                                beasts. He has
-                                formed a contract with the seal weapon known as Great Scythe Grynoth.
+                                Knight of Fellowship
+                                A knight of valor, he pours his heart and soul into protecting his homeland, his friends,
+                                and his comrades-in-arms.
+
                             </i>
                         </q>
                     </p>
@@ -163,22 +164,19 @@
                     <!--grynoth-->
                     <div v-if="alabardaVane">
                         <div class="table-title">
-                            <h2><i>Great Scythe Grynot.</i></h2>
+                            <h2><i>Alabarda.</i></h2>
                         </div>
                         <table class="table-fill">
                             <thead>
                                 <tr>
                                     <td class="tdinfoV">
-                                        Restricted file - code name: Grynoth. A massive scythe of lunar origin. This seal
-                                        weapon
-                                        harnesses ethereal darkness. While Grynoth augments its contractor's endurance, it
-                                        has a tendency
-                                        to over-amplify the wielder's animosity. Great care must be taken in choosing
-                                        Grynoth's
-                                        contractor.
+                                        For one aspiring youth, the halberd was the obvious weapon of choice. This one ins't
+                                        the fastest or sharpest around, but wielded correctly, it's nigh unbreakable. But
+                                        for him it's not about being the greatest. It's about fighting with friends on the
+                                        front lines and letting them know they'll never be alone.
                                     </td>
                                     <td>
-                                        <img class="grynoth" src="../assets/img/Grynoth.png" alt="Grynoth">
+                                        <img class="weapon" src="../assets/img/alabarda.png" alt="Alabarda">
                                     </td>
                                 </tr>
                                 <tr>
@@ -210,7 +208,6 @@
             </div>
         </div>
     </section>
-    
 </template>
 
 <script setup>
@@ -262,6 +259,16 @@ const alabardaOn = () => {
     margin-bottom: 10px;
 }
 
+.weapon {
+    width: 200%;
+    margin: auto;
+    display: flex;
+    border-radius: 20%;
+
+
+
+}
+
 .vaseragaB:hover {
     transition: all .3 ease;
     background-color: black;
@@ -269,6 +276,36 @@ const alabardaOn = () => {
     padding: 1em;
     border-radius: 10px;
 }
+
+
+.vaneB::before,
+.vaneB::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(to top, rgba(89,202,216,0.8) 0%, rgba(89,202,216,0) 70%);
+    background-position: bottom;
+    opacity: 0;
+    transition: opacity .3s ease, background-position .3s ease;
+}
+
+.vaneB::before {
+    top: 0;
+    left: 0;
+}
+
+.vaneB::after {
+    bottom: 0;
+    right: 0;
+}
+
+.vaneB:hover::before,
+.vaneB:hover::after {
+    opacity: 1;
+    background-position: top;
+}
+
 
 .vaseragaInfo {
     display: flex;
@@ -340,5 +377,129 @@ const alabardaOn = () => {
     }
 }
 
-/**/
-</style>
+/*.vaneB{
+    background: transparent;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all .3s ease;
+    width: 8%;
+}
+.vaneB:hover {
+    transition: all .3 ease;
+    background-image: linear-gradient(to top, rgba(89,202,216,0.8) 0%, rgba(89,202,216,0) 70%);
+    color: #ff9306;
+    border-radius: 10px;
+    
+    
+}*/
+.vaneB {
+    position: relative;
+    background: transparent;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all .3s ease;
+    width: 8%;
+}
+
+.vaneB::before,
+.vaneB::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(to top, rgba(89,202,216,0.8) 0%, rgba(89,202,216,0) 70%);
+    background-position: bottom;
+    opacity: 0;
+    border-radius: 10px;
+    transition: opacity .3s ease, background-position .3s ease;
+}
+
+.vaneB::before {
+    top: 0;
+    left: 0;
+}
+
+.vaneB::after {
+    bottom: 0;
+    right: 0;
+}
+
+.vaneB:hover::before,
+.vaneB:hover::after {
+    opacity: 1;
+    background-position: top;
+}
+
+.VaneInfo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(270deg, #9ac4c1, #f1e392, #ffffff, #c7520e);
+    background-size: 800% 800%;
+
+    -webkit-animation: FondoGradianteVane 17s ease infinite;
+    -moz-animation: FondoGradianteVane 17s ease infinite;
+    -o-animation: FondoGradianteVane 17s ease infinite;
+    animation: FondoGradianteVane 17s ease infinite;
+}
+
+/*ANIMACIONVANE*/
+@-webkit-keyframes FondoGradianteVane {
+    0% {
+        background-position: 0% 50%
+    }
+
+    50% {
+        background-position: 100% 50%
+    }
+
+    100% {
+        background-position: 0% 50%
+    }
+}
+
+@-moz-keyframes FondoGradianteVane {
+    0% {
+        background-position: 0% 50%
+    }
+
+    50% {
+        background-position: 100% 50%
+    }
+
+    100% {
+        background-position: 0% 50%
+    }
+}
+
+@-o-keyframes FondoGradianteVane {
+    0% {
+        background-position: 0% 50%
+    }
+
+    50% {
+        background-position: 100% 50%
+    }
+
+    100% {
+        background-position: 0% 50%
+    }
+}
+
+@keyframes FondoGradianteVane {
+    0% {
+        background-position: 0% 50%
+    }
+
+    50% {
+        background-position: 100% 50%
+    }
+
+    100% {
+        background-position: 0% 50%
+    }
+}
+
+/**/</style>
